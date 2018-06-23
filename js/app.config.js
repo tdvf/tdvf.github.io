2dccd04fdcbb092b0a8151035b1f7e1c28ef48d8
tdvf <info.tdvf@gmail.com>
2018-06-22 19:01:09 -0500

    committing for github pages

---
+++ js/app.config.js
@@ -0,0 +1,19 @@
+var app = angular.module("tdvfApp", ["ngRoute"]);
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/home", {
+        template : "<iframe src='html/home.html' style='border:none' width=100% scrolling='no' onload='resizeIframe(this)'></iframe>"
+    })
+    .when("/events", {
+        template : "<iframe src='html/Events.html' style='border:none' width=100% scrolling='no' onload='resizeIframe(this)'></iframe>"
+    })
+    .when("/contact", {
+            template : "<iframe src='html/contact-us.html' style='border:none' width=100% scrolling='no' onload='resizeIframe(this)'></iframe>"
+        })
+	.otherwise("/home");
+});
+
+
+  function resizeIframe(obj) {
+    obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
+  }
